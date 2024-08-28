d66e9b3f28acb2721b19e8662c4cc1d699bad4d4
FAJRI2378 <armanfajri008@gmail.com>
2024-08-28 15:51:44 +0700

    commit type

---
+++ app.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("input");
+const addButton = document.querySelector(".add-button");
+const todosHtml = document.querySelector(".todos");
+const emptyImage = document.querySelector(".empty-image");
+let todosJson = JSON.parse(localStorage.getItem("todos")) || [];
+const deleteAllButton = document.querySelector(".delete-all");
+const filters = document.querySelectorAll(".filter");
+
+todoButton.addEventListener('click', function (e) {
+    e.prevent
+}
+)
